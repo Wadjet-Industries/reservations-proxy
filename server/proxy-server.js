de55c71844e72d9208400ac0685cf22427d5ce41
Wes <wesley.qb.wen@gmail.com>
2019-09-23 19:09:52 -0700

    Changed get redirect path to absolute of service on ec2

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -15,15 +15,8 @@ app.use('/restaurants/:id', express.static('public'));
 
 
 app.get('/api/restaurants/:id' , (req, res) => {
-  res.redirect(`http://localhost:3002/api/restaurants/${req.params.id}`);
+  res.redirect(`ec2-3-15-165-218.us-east-2.compute.amazonaws.com/api/restaurants/${req.params.id}`);
 })
 
 
 app.listen(port, () => { console.log(`Proxy server listening on port ${port}`); });
-
-// app.use('/restaurants/:id' , proxy('http://localhost:3000' , {
-//   // console.log('Currently in proxy: Re-routing to service')
-//   forwardPath: function(req, res) {
-//     return '' + req.url
-//   }
-// }))
